946ea530b64c5bfd83b60423525787530b6fcfa7
swapna <swapnasiri.com>
2025-02-06 14:14:47 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,17 @@
  #output "subnets_info" {
   #  value  = aws_subnet.public
  #}
+
+ output  "vpc_id" {
+    value = aws_vpc.main.id
+ }
+ output  "public_subnet_id" {
+    value = aws_subnet.public[*]id
+ }
+
+ output  "private_subnet_id" {
+    value = aws_subnet.private[*].id
+ }
+ output  "database_subnet_id" {
+    value = aws_subnet.database[*].id
+ }
